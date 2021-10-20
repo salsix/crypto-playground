5a83b04d8074942c8177b3231273af463f55f4ab
Jonathan Salzer <jonathan.salzer@timebite.at>
2021-10-20 18:58:45 +0200

    use env var for api key

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
+from dotenv import load_dotenv
+load_dotenv()
 
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 parameters = {
@@ -9,21 +12,19 @@ parameters = {
 }
 headers = {
   'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': 'a6952186-cdb5-4263-9d7c-250c9f62ad6a',
+  'X-CMC_PRO_API_KEY': os.environ['X-CMC_PRO_API_KEY'],
 }
 
-session = Session()
-session.headers.update(headers)
-
-try:
-  response = session.get(url, params=parameters)
-  data = json.loads(response.text)
-  print(data['data']['1']['quote']['EUR']['price'])
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-  print(e)
-
 def test():
-    print('Hi,')
+    session = Session()
+    session.headers.update(headers)
+
+    try:
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        print('Bitcoin: ' + str(round(data['data']['1']['quote']['EUR']['price'],2)) + '€')
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)
 
 
 if __name__ == '__main__':
